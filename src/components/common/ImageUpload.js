ba38eca4aca4b2c178a93c093576a2304af90f6b
abigailforeman <forea005@gmail.com>
2020-03-12 18:07:35 +0000

    added env file

--- src/components/common/ImageUpload.js
+++ src/components/common/ImageUpload.js
@@ -14,7 +14,7 @@ class ImageUpload extends React.Component {
     console.log(process.env.CLOUDINARY_USER_KEY)
     console.log(process.env.PORT)
     console.log(process.env.MONGODB_URI)
-    data.append('upload_preset', 'hmrrfxib') // this is your user key from your cloudinary account
+    data.append('upload_preset', 'j7lqzji3') // this is your user key from your cloudinary account
     const res = await axios.post(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_USER_KEY}/image/upload`, data)
     this.setState({ image: res.data.url, original: files[0].name }, () => {
       this.props.handleChangeImage({ target: { name: this.props.fieldName, value: res.data.url } }) 
